c4f4f379366961128821e9fdcc69ea5e7e1b81f6
kubrakaradirek <karadirekkubra96@gmail.com>
2024-06-25 12:26:00 +0300

    Admin theme integrated. Navbar, sidebar, and scripts separated using partial views. Car list prepared and displayed in admin panel. New car entry page designed.

---
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7284/api/Cars/GetCarWithBrand");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCar()
+        {
+            return View();
+        }
+    }
+}
