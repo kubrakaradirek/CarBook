09f6a730a96d6e84dbacfa53013d8cf985aadfea
kubrakaradirek <karadirekkubra96@gmail.com>
2024-07-01 09:48:23 +0300

    Brands added to car features using dropdown. Car add and update page designs completed. Additionally, CRUD operations for cars were implemented.

--- Presentation/CarBook.WebApi/Controllers/CarsController.cs
+++ Presentation/CarBook.WebApi/Controllers/CarsController.cs
@@ -50,7 +50,7 @@ namespace CarBook.WebApi.Controllers
             return Ok("Araba bilgisi başarıyla eklendi.");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveCar(int id)
         {
             await _removeCarCommandHandler.Handle(new RemoveCarCommand(id));
