07b64b21e26daf43d8216b68d5a6de0518ac74f4
kubrakaradirek <karadirekkubra96@gmail.com>
2024-07-12 19:58:29 +0300

    Statistics on the homepage have been fetched. Footer adjustments were made. Sweet Alert was used. Menus were completed.

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,11 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultStatisticsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            #region-TotalCarCountStatistic
+            var responseMessage = await client.GetAsync("https://localhost:7284/api/Statistics/GetCarCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                ViewBag.carCount = values.CarCount;
+            }
+            #endregion
+
+            #region-TotalLocationCountStatistic
+            var responseMessage2 = await client.GetAsync("https://localhost:7284/api/Statistics/GetLocationCount");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData2);
+                ViewBag.locationCount = values2.LocationCount;
+            }
+            #endregion
+
+            #region-TotalBrandCountStatistic
+            var responseMessage3 = await client.GetAsync("https://localhost:7284/api/Statistics/GetBrandCount");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData3);
+                ViewBag.brandCount = values3.BrandCount;
+            }
+            #endregion
+
+            #region-CarCountByFuelGasolineOrDieselStatistic
+            var responseMessage4 = await client.GetAsync("https://localhost:7284/api/Statistics/GetCarCountByFuelElectric");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData4);
+                ViewBag.carCountByFuelElectric = values4.carCountByFuelElectric;
+            }
+            #endregion
+
+
             return View();
         }
     }
--- Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.FooterAddressDtos;
+using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _FooterUILayoutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7284/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
